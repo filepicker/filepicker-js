77ddc533de61bb22366ea900801d6394d6ce32ab
François de Metz <francois@2metz.fr>
2015-12-23 14:01:13 +0100

    Remove isSafari, isMobileSafari and isIe7 methods.

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -14,21 +14,6 @@ filepicker.extend('browser', function(){
         return !!navigator.userAgent.match(/Android/i);
     };
 
-    var isIE7 = function() {
-        return !!navigator.userAgent.match(/MSIE 7\.0/i);
-    };
-
-    var isSafari = function() {
-        return (navigator.userAgent.search('Safari') >= 0 &&
-            navigator.userAgent.search('Chrome') < 0);
-    };
-    
-    var isMobileSafari = function() {
-        return !!(navigator.userAgent.match(/(iPod|iPhone|iPad)/) && 
-            navigator.userAgent.match(/AppleWebKit/) && 
-            !navigator.userAgent.match(/CriOS/));
-    };
-
     var getLanguage = function() {
         var language = window.navigator.userLanguage || window.navigator.language;
         if (language === undefined) {
@@ -40,13 +25,9 @@ filepicker.extend('browser', function(){
 
     var isMobile = isIOS() || isAndroid();
     
-
     return {
         isIOS: isIOS,
         isAndroid: isAndroid,
-        isIE7: isIE7,
-        isSafari: isSafari,
-        isMobileSafari: isMobileSafari,
         getLanguage: getLanguage,
         isMobile: isMobile
     };
